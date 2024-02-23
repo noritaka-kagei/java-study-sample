9c7f01e99a0f4fac03f6e6a3444aa6a42304e9ee
keronori <it0ka4ge8i@gmail.com>
2024-02-23 17:00:31 +0900

    remove extra import definition

--- src/test/java/noritakakagei/study/testing/ProductServiceTest.java
+++ src/test/java/noritakakagei/study/testing/ProductServiceTest.java
@@ -2,7 +2,6 @@ package noritakakagei.study.testing;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.internal.Strings;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
