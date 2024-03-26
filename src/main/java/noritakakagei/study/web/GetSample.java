11941bab8a547dd8c5217f465c15cfc36d25d9ce
it0ka4ge8i@gmail.com <it0ka4ge8i@gmail.com>
2024-03-26 19:39:23 +0900

    sample code of sending http get request

---
+++ src/main/java/noritakakagei/study/web/GetSample.java
@@ -0,0 +1,27 @@
+package noritakakagei.study.web;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class GetSample {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+        // HttpClientのインスタンスを作成
+        HttpClient client = HttpClient.newHttpClient();
+
+        // HttpRequestのインスタンスを作成
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://gihyo.jp/book/2014/978-4-7741-6685-8")) // ここにリクエストしたいURLを指定
+                .GET() // HTTP GETリクエストを指定
+                .build();
+
+        // リクエストを送信し、レスポンスを受け取る
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // レスポンスボディを出力
+        System.out.println(response.body());
+    }
+}
